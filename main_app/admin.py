637a4e53255f463d2be38e7c42d7f700250c8d37
rayjim28 <rjimenez1030@icloud.com>
2023-06-07 00:04:10 -0400

    Adding Auth and photos

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Cohorts
-
 from .models import Cohorts, Student
+
+from .models import Cohorts, Student, Student_Grades, Photo
 
 
 # Register your models here.
-
-
-admin.site.register(Student) 
 admin.site.register(Cohorts)
+admin.site.register(Student) 
+admin.site.register(Student_Grades) 
+admin.site.register(Photo) 
